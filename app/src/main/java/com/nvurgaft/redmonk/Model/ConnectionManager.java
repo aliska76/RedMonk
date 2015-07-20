7d60926240d3c39879df3c8733e91777a3583a1d
nvurgaft <slipperygate@gmail.com>
2015-07-20 17:48:55 +0300

    Added an empty widget stub with configuration activity (and layout markups) Changed default widget preview Minor changes in SqlAccess class

--- app/src/main/java/com/nvurgaft/redmonk/Model/ConnectionManager.java
+++ app/src/main/java/com/nvurgaft/redmonk/Model/ConnectionManager.java
@@ -2,6 +2,9 @@ package com.nvurgaft.redmonk.Model;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.nvurgaft.redmonk.Values;
 
 /**
  * Created by Koby on 02-Jul-15.
@@ -18,6 +21,18 @@ public class ConnectionManager {
     }
 
     public static void closeConnection() {
+        try {
+            if (sqlAccess != null) {
+                sqlAccess.close();
+                Log.d(Values.LOG, "Closing SQLite Connection");
+                sqlAccess = null;
+            }
+        } catch (Exception ex) {
+            Log.e(Values.LOG, "Error encountered while closing SQLite connection", ex);
+        }
+    }
+
+    public static void closeConnectionSilently() {
         sqlAccess.close();
         sqlAccess = null;
     }
